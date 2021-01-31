3d2b5e5304e1615af11cea84ac54d1cd515f1b74
johbra90 <johbra90@bitbucket.org>
2021-01-31 20:08:37 +0100

    some changes

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -37,7 +37,9 @@ class DatabaseSeeder extends Seeder
                     new Sequence(
                         ['type' => 'html'],
                         ['type' => 'image'],
-                        ['type' => 'map'],
+                        ['type' => 'map', 'content' => '{"coordinates":[{"id":"Neu Delhi","position":{"lat":28.61584308390754,"lng":77.21540990624999},"tmp":"4"},  				{"id":"\r\nAgra","position":{"lat":27.17399791074048,"lng":78.01741185937499},"tmp":"4"},
+                        {"id":"Jaipur","position":{"lat":26.914695040529676,"lng":75.79268041406249},"tmp":"4"}],
+                                    "zoomlevel":7,"connection":"0"}'],
                         ['type' => 'video']
                     )
                 )
